8b08142ab23da10741a028e536bb6fdf472f3610
João Vitor Bessa Lacerda <80555195+jvjzn@users.noreply.github.com>
2023-05-01 18:10:08 -0300

    Update Aplicacao.java

--- codigo/src/app/Aplicacao.java
+++ codigo/src/app/Aplicacao.java
@@ -2,7 +2,7 @@ package app;
 
 public class Aplicacao {
 
-	public static void carregarDadosS(Serie[] vetorDeSeries) {
+	public static void carregarDadosS(Stream[] vetorDeSeries) {
 		ArquivoTextoLeitura file = new ArquivoTextoLeitura("POO_Series.csv");
 
 		String dadosS = file.ler();
@@ -22,7 +22,7 @@ public class Aplicacao {
 		file.fecharArquivo();
 	}
 
-	public static void carregarDadosF(Filme[] vetorDeFilmes) {
+	public static void carregarDadosF(Stream[] vetorDeFilmes) {
 		ArquivoTextoLeitura file = new ArquivoTextoLeitura("POO_Filmes.csv");
 
 		String dadosF = file.ler();
@@ -35,7 +35,7 @@ public class Aplicacao {
 
 		while (dadosF != null) {
 
-			Filme novoFilme = new Filme(Integer.parseInt(dadosSeparadosF[0]),dadosSeparadosF[1],dadosSeparadosF[2],Integer.parseInt(dadosSeparadosF[3]));
+			Stream novoFilme = new Filme(Integer.parseInt(dadosSeparadosF[0]),dadosSeparadosF[1],dadosSeparadosF[2],Float.parseFloat(dadosSeparadosF[3]));
 			vetorDeFilmes[cont++] = novoFilme;
 			dadosF = file.ler();
 		}
@@ -43,7 +43,7 @@ public class Aplicacao {
 		file.fecharArquivo();
 	}
 
-	public static void carregarDadosA(Cliente[] vetorDeEspectadores, Serie[] vetorDeSerie) {
+	public static void carregarDadosA(Cliente[] vetorDeEspectadores, Stream[] vetorDeSerie, PlataformaStreaming plataforma) {
 		ArquivoTextoLeitura file = new ArquivoTextoLeitura("POO_Audiencia.csv");
 
 		String dadosA = file.ler();
@@ -54,12 +54,13 @@ public class Aplicacao {
 		while (dadosA != null) {
 			for (Cliente cliente : vetorDeEspectadores) {
 				if (dadosSeparadosA[0].equals(cliente.getLogin())) {
-					for (Serie serie : vetorDeSerie) {
-						if (serie.getIdSerie == dadosSeparadosA[2]) {
+					for (Stream serie : vetorDeSerie) {
+						if (serie.getId() == Integer.parseInt(dadosSeparadosA[2])) {
 							if (dadosSeparadosA[1].equals("F")) {
 								cliente.adicionarNaListaParaVer(serie);
 							} else /* dadosSeparadosA[1] == "A" */ {
 								cliente.adicionarNaListaJaVisto(serie);
+								plataforma.registrarAudiencia(serie);
 							}
 						}
 					}
@@ -91,150 +92,115 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 
+		//PLATAFORMA
+		//Criando plataforma de streaming "Amaze"
+		PlataformaStreaming Amaze = new PlataformaStreaming("Amaze");
+
 		//ESPECTADORES
 		//Criando vetor de clientes
 		Cliente[] vetorClientes = new Cliente[51893];
 		//Carregando dados do arquivo "POO_Espectadores.csv" para o vetor de Series
 		carregarDadosE(vetorClientes);
-
-		//SERIE
-		//Criando vetor de series
-		Serie[] vetorSeries = new Serie[131];
-		//Carregando dados do arquivo "POO_Series.csv" para o vetor de Series
-		carregarDadosS(vetorSeries);
-
-		//FILME
-		//Criando vetor de filmes
-		Filme[] vetorFilmes = new Filme[203];
-		//Carregando dados do arquivo "POO_Filmes.csv" para o vetor de Series
-		carregarDadosF(vetorFilmes);
-
-
-		//PLATAFORMA
-		//Criando plataforma de streaming "Amaze"
-		PlataformaStreaming Amaze = new PlataformaStreaming("Amaze");
-		//Adicionando *series* à Amaze
-		for (Serie serie : vetorSeries) {
-			Amaze.adicionarSerie(serie);
-		}
-		//Adicionando *filmes* à Amaze
-		for (Filme filme : vetorFilmes) {
-			Amaze.adicionarFilme(filme);
-		}
 		//Adicionando *clientes* à Amaze
 		for (Cliente cliente : vetorClientes) {
 			Amaze.adicionarCliente(cliente);
 		}
 
-		//AUDIENCIA
+		//SERIE
+		//Criando vetor colecao de series
+		Stream[] vetorSeries = new Stream[131];
+		//Carregando dados do arquivo "POO_Series.csv" para o vetor de colecao
+		carregarDadosS(vetorSeries);
+		//Adicionando *series* à Amaze
+		for (Stream serie : vetorSeries) {
+			Amaze.adicionarColecao(serie);
+		}
+
+		//AUDIENCIA (SERIES)
 		//Carregando dados do arquivo "POO_Audiencia.csv"
-		carregarDadosA(vetorClientes,vetorSeries);
+		carregarDadosA(vetorClientes,vetorSeries,Amaze);
+
+
+		//FILME
+		//Criando vetor de filmes
+		Stream[] vetorFilmes = new Filme[203];
+		//Carregando dados do arquivo "POO_Filmes.csv" para o vetor de Series
+		carregarDadosF(vetorFilmes);
+		//Adicionando *filmes* à Amaze
+		for (Stream filme : vetorFilmes) {
+			Amaze.adicionarColecao(filme);
+		}
 
 		//REALIZAR LOGIN
 		Amaze.login("Ada10","ABro14415");
-
 
 		int op;
 		do {
 			System.out.println("=-"+ Amaze.getNome() +"-=");
 			System.out.println("Olá " + Amaze.getNomeClienteAtual() + "!");
 			System.out.println("Digite uma das opções abaixo:");
-			System.out.println("[1]Catálogo"); //Adicionar series "para assistir"
+			System.out.println("[1]Catálogo"); //Pesquisar series e filmes -> Aicionar "para assistir" ou "já assistido"
 			System.out.println("[2]Perfil"); //Marcar series "já assistidas" e retornar "lista de series ja assistidas"
+			System.out.println("[3]Adicionar série ou filme ao catálogo");
 			System.out.println("[0]Finalizar programa");
 			System.out.print(">> ");
 			op = MyIO.readInt();
 
 			switch (op) {
 				case 1:
-					System.out.println("[1]Pesquisar série");
-					System.out.println("[2]Pesquisar filme");
+					System.out.println("-Pesquisar Series e Filmes-");
+					System.out.println("Filtrar por:");
+					System.out.println("[1]Nome");
+					System.out.println("[2]Gênero");
+					System.out.println("[3]Idioma");
+					System.out.print(">> ");
 					int op1 = MyIO.readInt();
+
+					Stream opcaoEncontrada;
+
+					switch (op1) {
+						case 1:
+							System.out.print("Digite o nome: ");
+							String nome = MyIO.readString();
+							Amaze.filtrarPorNome(nome);
+
+							break;
+
+						case 2:
+							System.out.print("Digite o gênero: ");
+							String genero = MyIO.readString();
+							Amaze.filtrarPorGenero(genero);
+
+							break;
+						case 3:
+							System.out.print("Digite o idioma: ");
+							String idioma = MyIO.readString();
+							Amaze.filtrarPorIdioma(idioma);
+
+							break;
+						default:
+							System.out.println("Opção inválida. Tente novamente.");
+					}
+					System.out.println("[1]Marcar como opção como: já visto(a)");
+					System.out.println("[2]Adicionar opção à lista: assistir futuramente");
+					System.out.println("[0]Sair");
+					op1 = MyIO.readInt();
 
 					switch (op1){
 						case 1:
-							System.out.println("Filtrar por:");
-							System.out.println("[1]Nome");
-							System.out.println("[2]Gênero");
-							System.out.println("[3]Idioma");
-							System.out.println("[4]Quantidade de Episódios");
-							System.out.print(">> ");
-							op1 = MyIO.readInt();
-
-							Serie serieEncontrada;
-
-							switch (op1) {
-								case 1:
-									System.out.print("Digite o nome da série: ");
-									String nome = MyIO.readString();
-									serieEncontrada = Amaze.filtrarSeriesPorNome(nome);
-									break;
-
-								case 2:
-									System.out.print("Digite o gênero: ");
-									String genero = MyIO.readString();
-									serieEncontrada = Amaze.filtrarSeriesPorGenero(genero);
-									break;
-								case 3:
-									System.out.print("Digite o idioma: ");
-									String idioma = MyIO.readString();
-									serieEncontrada = Amaze.filtrarSeriesPorIdioma(idioma);
-									break;
-								case 4:
-									System.out.print("Digite a quantidade de episódios: ");
-									int qntEp = MyIO.readInt();
-									serieEncontrada = Amaze.filtrarSeriesPorQtdEpisodios(qntEp);
-									break;
-								default:
-									System.out.println("Opção inválida.");
-							}
-							System.out.println("[1]Marcar como série já vista");
-							System.out.println("[2]Adicionar a série à lista para assistir futuramente");
-							System.out.println("[0]Sair");
-							op1 = MyIO.readInt();
-
-							switch (op1){
-								case 1:
-									Amaze.getClienteAtual().adicionarNaListaJaVisto(serieEncontrada);
-									break;
-								case 2:
-									Amaze.getClienteAtual().adicionarNaListaParaVer(serieEncontrada);
-									break;
-								case 0:
-									System.out.println("Finalizando programa.");
-									break;
-								default:
-									System.out.println("Opção inválida.");
-							}
+							Amaze.getClienteAtual().adicionarNaListaJaVisto(opcaoEncontrada);
 							break;
 						case 2:
-							System.out.println("Filtrar por:");
-							System.out.println("[1]Nome");
-							System.out.println("[2]Gênero");
-							System.out.println("[3]Idioma");
-							System.out.print(">> ");
-							op1 = MyIO.readInt();
-							
-							Filme filmeEncontrado;
-							
-							switch (op1){
-								case 1:
-									
-									break;
-								case 2:
-									
-									break;
-								case 3:
-									
-									break;
-								default:
-									System.out.println("Opção inválida.");
-							}
-							
+							Amaze.getClienteAtual().adicionarNaListaParaVer(opcaoEncontrada);
+							break;
+						case 0:
+							System.out.println("Finalizando programa.");
 							break;
 						default:
-							System.out.println("Opção inválida.");
+							System.out.println("Opção inválida. Tente novamente.");
 					}
+					break;
+
 					break;
 				case 2:
 					System.out.println("Selecione uma das opções abaixo: ");
@@ -251,7 +217,7 @@ public class Aplicacao {
 							Amaze.getClienteAtual().mostrarListaJaVista();
 							break;
 						default:
-							System.out.println("Opção inválida.");
+							System.out.println("Opção inválida. Tente novamente.");
 					}
 					break;
 				case 3:
@@ -262,7 +228,7 @@ public class Aplicacao {
 					System.out.println("Finalizando programa.");
 					break;
 				default:
-					System.out.println("Opção inválida.");
+					System.out.println("Opção inválida. Tente novamente.");
 			}
 
 		}while (op != 3);
