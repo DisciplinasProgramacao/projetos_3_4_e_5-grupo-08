7167b447eade76fe732fb467e2eb51ddb5015e7a
Nathan <72050032+nATHANnSil@users.noreply.github.com>
2023-05-13 14:32:02 -0300

    Update ClienteTest.java

--- codigo/src/tests/ClienteTest.java
+++ codigo/src/tests/ClienteTest.java
@@ -1,84 +1,109 @@
-package tests;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import app.Cliente;
-import app.Serie;
-import app.Stream;
-
-
 public class ClienteTest {
-    private Cliente cliente;
-	private Serie serie1;
-	private Serie serie2;
-	
-	@BeforeEach
-	public void setUp() {
-		// Configuração inicial para os testes
-		cliente = new Cliente("usuario", "senha", "login");
-		serie1 = new Serie(1,"Stream1", "22-01-2020");
-		serie2 = new Serie(2,"Stream2", "22-01-2020");
-	}
-	
-	// @Test
-	// public void testAdicionarNaLista() {
-	// 	cliente.adicionarNaLista(serie1);
-	// 	assertEquals(1, cliente.listaParaVer.size());
-	// 	assertEquals(cliente.listaParaVer.contains(serie1));
-	// }
-	
-	// @Test
-	// public void testRetirarDaLista() {
-	// 	cliente.adicionarNaLista(serie1);
-	// 	cliente.adicionarNaLista(serie2);
-	// 	assertEquals(2, cliente.listaParaVer.size());
-	// 	cliente.retirarDaLista("Stream1");
-	// 	assertEquals(1, cliente.listaParaVer.size());
-	// 	assertFalse(cliente.listaParaVer.contains(serie1));
-	// 	assertTrue(cliente.listaParaVer.contains(serie2));
-	// }
-	
-	@Test
-	public void testFiltrarPorGenero() {
-		cliente.registrarAudiencia(serie1);
-		cliente.adicionarNaListaParaVer(serie2);
-		List<Stream> listaFiltrada = cliente.filtrarPorGenero("Genero1");
-		assertEquals(1, listaFiltrada.size());
-		assertTrue(listaFiltrada.contains(serie1));
-		assertFalse(listaFiltrada.contains(serie2));
-	}
-	
-	@Test
-	public void testFiltrarPorIdioma() {
-		cliente.registrarAudiencia(serie1);
-		cliente.adicionarNaListaParaVer(serie2);
-		List<Stream> listaFiltrada = cliente.filtrarPorIdioma("Idioma1");
-		assertEquals(1, listaFiltrada.size());
-		assertTrue(listaFiltrada.contains(serie1));
-		assertFalse(listaFiltrada.contains(serie2));
-	}
-	
-	// @Test
-	// public void testFiltrarPorQtdEpisodios() {
-	// 	cliente.registrarAudiencia(serie1);
-	// 	cliente.adicionarNaLista(serie2);
-	// 	List<Stream> listaFiltrada = cliente.filtrarPorQtdEpisodios(10);
-	// 	assertEquals(1, listaFiltrada.size());
-	// 	assertTrue(listaFiltrada.contains(serie1));
-	// 	assertFalse(listaFiltrada.contains(serie2));
-	// }
-	
-	// @Test
-	// public void testRegistrarAudiencia() {
-	// 	cliente.registrarAudiencia(serie1);
-	// 	assertEquals(1, cliente.listaJaVistas.size());
-	// 	assertTrue(cliente.listaJaVistas.contains(serie1));
-	// }
+
+    @Test
+    public void testarCriarCliente() {
+        Cliente cliente = new Cliente("joao123", "joao@example.com", "senha123");
+        Assertions.assertEquals("joao123", cliente.getNomeDeUsuario());
+        Assertions.assertEquals("joao@example.com", cliente.getLogin());
+        Assertions.assertEquals("senha123", cliente.getSenha());
+        Assertions.assertNotNull(cliente.mostrarListaJaVista());
+        Assertions.assertNotNull(cliente.mostrarListaParaAssistir());
+    }
+
+    @Test
+    public void testarAdicionarNaListaParaVer() {
+        Cliente cliente = new Cliente("joao123", "joao@example.com", "senha123");
+        Filme filme = new Filme(1, "Titanic", "Drama", "Inglês", "1997-12-19", 194.5f);
+        cliente.adicionarNaListaParaVer(filme);
+        List<Stream> listaParaVer = cliente.mostrarListaParaAssistir();
+        Assertions.assertEquals(1, listaParaVer.size());
+        Assertions.assertEquals(filme, listaParaVer.get(0));
+    }
+
+    @Test
+    public void testarAdicionarNaListaJaVisto() {
+        Cliente cliente = new Cliente("joao123", "joao@example.com", "senha123");
+        Filme filme = new Filme(1, "Titanic", "Drama", "Inglês", "1997-12-19", 194.5f);
+        cliente.adicionarNaListaJaVisto(filme);
+        List<AvaliacaoStream> listaJaVistas = cliente.mostrarListaJaVista();
+        Assertions.assertEquals(1, listaJaVistas.size());
+        Assertions.assertEquals(filme, listaJaVistas.get(0).getStream());
+    }
+
+    @Test
+    public void testarRetirarDaLista() {
+        Cliente cliente = new Cliente("joao123", "joao@example.com", "senha123");
+        Filme filme = new Filme(1, "Titanic", "Drama", "Inglês", "1997-12-19", 194.5f);
+        cliente.adicionarNaListaParaVer(filme);
+        cliente.retirarDaLista("Titanic");
+        List<Stream> listaParaVer = cliente.mostrarListaParaAssistir();
+        Assertions.assertEquals(0, listaParaVer.size());
+    }
+
+    @Test
+    public void testarFiltrarPorGenero() {
+        Cliente cliente = new Cliente("joao123", "joao@example.com", "senha123");
+        Filme filme1 = new Filme(1, "Titanic", "Drama", "Inglês", "1997-12-19", 194.5f);
+        Filme filme2 = new Filme(2, "Interestelar", "Ficção Científica", "Inglês", "2014-11-07", 169.0f);
+        cliente.adicionarNaListaJaVisto(filme1);
+        cliente.adicionarNaListaParaVer(filme2);
+        List<Stream> filmesDrama = cliente.filtrarPorGenero("Drama");
+        List<Stream> filmesFiccao = cliente.filtrarPorGenero("Ficção Científica");
+        Assertions.assertEquals(1, filmesDrama.size());
+        Assertions.assertEquals(1, filmesFiccao.size());
+        Assertions.assertEquals(filme1, filmesDrama.get(0));
+        Assertions.assertEquals(filme2, filmesFiccao.get(0));
+    }
+
+    @Test
+    public void testarFiltrarPorIdioma() {
+        Cliente cliente = new Cliente("joao123", "joao@example.com", "senha123");
+        Filme filme1 = new Filme(1, "Titanic", "Drama", "Inglês", "1997-12-19", 194.5f);
+        Filme filme2 = new Filme(2, "Interestelar", "Ficção Científica", "Inglês", "2014-11-07", 169.0f);
+        cliente.adicionarNaListaJaVisto(filme1);
+        cliente.adicionarNaListaParaVer(filme2);
+        List<Stream> filmesIngles = cliente.filtrarPorIdioma("Inglês");
+        Assertions.assertEquals(2, filmesIngles.size());
+        Assertions.assertEquals(filme1, filmesIngles.get(0));
+        Assertions.assertEquals(filme2, filmesIngles.get(1));
+    }
+
+    @Test
+    public void testarAvaliar() {
+        Cliente cliente = new Cliente("joao123", "joao@example.com", "senha123");
+        Filme filme = new Filme(1, "Titanic", "Drama", "Inglês", "1997-12-19", 194.5f);
+        cliente.adicionarNaListaJaVisto(filme);
+        cliente.avaliar(1, 4.5f);
+        List<AvaliacaoStream> listaJaVistas = cliente.mostrarListaJaVista();
+        Assertions.assertEquals(1, listaJaVistas.size());
+        Assertions.assertEquals(4.5f, listaJaVistas.get(0).getAvaliacao());
+    }
+
+    @Test
+    public void testarRegistrarAudiencia() {
+        Cliente cliente = new Cliente("joao123", "joao@example.com", "senha123");
+        Filme filme = new Filme(1, "Titanic", "Drama", "Inglês", "1997-12-19", 194.5f);
+        cliente.registrarAudiencia(filme);
+        List<AvaliacaoStream> listaJaVistas = cliente.mostrarListaJaVista();
+        Assertions.assertEquals(1, listaJaVistas.size());
+        Assertions.assertEquals(filme, listaJaVistas.get(0).getStream());
+    }
+
+    @Test
+    public void testarAvaliados() {
+        Cliente cliente = new Cliente("joao123", "joao@example.com", "senha123");
+        Filme filme1 = new Filme(1, "Titanic", "Drama", "Inglês", "1997-12-19", 194.5f);
+        Filme filme2 = new Filme(2, "Interestelar", "Ficção Científica", "Inglês", "2014-11-07", 169.0f);
+        cliente.adicionarNaListaJaVisto(filme1);
+        cliente.adicionarNaListaParaVer(filme2);
+        cliente.avaliar(1, 4.5f);
+        List<Stream> filmesAvaliados = cliente.avaliados();
+        Assertions.assertEquals(1, filmesAvaliados.size());
+        Assertions.assertEquals(filme1, filmesAvaliados.get(0));
+    }
 }
